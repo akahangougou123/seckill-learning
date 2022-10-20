0d431eca89525f9353bbd2f782b60346a9ce2ca0
akahangougou123 <onthemisson@163.com>
2022-10-20 23:32:25 +0800

    redis配置类

--- src/main/java/com/han/seckill/config/RedisConfig.java
+++ src/main/java/com/han/seckill/config/RedisConfig.java
@@ -1,7 +1,30 @@
 package com.han.seckill.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * redis配置类
+ */
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        //设置rediskey的序列化
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置value的序列化
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        //hash类型的key value序列化
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        //注入连接工厂
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
 }
