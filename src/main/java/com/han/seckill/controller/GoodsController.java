e41d2fdf3225d27ca9a33c4b0a5a2d49ba306ce0
akahangougou123 <onthemisson@163.com>
2022-10-21 14:26:04 +0800

    优化登录功能

--- src/main/java/com/han/seckill/controller/GoodsController.java
+++ src/main/java/com/han/seckill/controller/GoodsController.java
@@ -1,6 +1,8 @@
 package com.han.seckill.controller;
 
 import com.han.seckill.pojo.User;
+import com.han.seckill.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -8,19 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
 
+    @Autowired
+    private IUserService userService;
+
     @RequestMapping("/toList")
-    public String toList(HttpSession session, Model model, @CookieValue("userTicket")String ticket){
+    public String toList(HttpServletRequest request, HttpServletResponse response, Model model, @CookieValue("userTicket")String ticket){
         System.out.println(ticket);
         if(StringUtils.isEmpty(ticket)){
             return "login";
         }
-        User user = (User) session.getAttribute(ticket);
+//        User user = (User) session.getAttribute(ticket);
+        User user =  userService.getUserByCookie(ticket, request, response);
         System.out.println("user:"+user);
         if(null == user){
             return "login";
