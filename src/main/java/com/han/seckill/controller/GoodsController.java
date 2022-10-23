fe62efb3c6d6284ba0f642f4ab8a778bee9ed844
akahangougou123 <onthemisson@163.com>
2022-10-23 13:59:28 +0800

    跳转商品详情页

--- src/main/java/com/han/seckill/controller/GoodsController.java
+++ src/main/java/com/han/seckill/controller/GoodsController.java
@@ -3,10 +3,12 @@ package com.han.seckill.controller;
 import com.han.seckill.pojo.User;
 import com.han.seckill.service.IGoodsService;
 import com.han.seckill.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.thymeleaf.util.StringUtils;
@@ -17,6 +19,7 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/goods")
+@Slf4j
 public class GoodsController {
 
     @Autowired
@@ -39,5 +42,19 @@ public class GoodsController {
         model.addAttribute("user",user);
         model.addAttribute("goodsList",goodsService.findGoodVo());
         return "goodsList";
+    }
+
+    /**
+     * 跳转商品详情页
+     * @param model
+     * @param user
+     * @param goodsId
+     * @return
+     */
+    @RequestMapping("/toDetail/{goodsId}")
+    public String toDetail(Model model, User user, @PathVariable Long goodsId){
+        model.addAttribute("user",user);
+        model.addAttribute("goods",goodsService.findGoodVoByGoodsId(goodsId));
+        return "goodsDetail";
     }
 }
